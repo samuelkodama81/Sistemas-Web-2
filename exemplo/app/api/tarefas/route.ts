4b7e1000535b2fd9d4c748604ff48cc5f07bf17b
S4MK991 <samuel.etec018@gmail.com>
2025-04-11 07:43:44 -0300

    route.ts

--- exemplo/app/api/tarefas/route.ts
+++ exemplo/app/api/tarefas/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET()  {
-    return NextResponse.json({mensagem : "Olá Mundo!!!"})
+    return NextResponse.json(
+        {
+            mensagem : "Olá Mundo!!!"
+        }
+    )
 }
