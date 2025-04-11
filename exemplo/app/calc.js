e49d60fc4096f4ed3247f9014a1b3215177c827b
S4MK991 <samuel.etec018@gmail.com>
2025-04-11 07:51:15 -0300

    calc.js

--- exemplo/app/calc.js
+++ exemplo/app/calc.js
@@ -1,1 +1,12 @@
+import { NextResponse } from "next/server";
 
+export async function GET() {
+  const result = [];
+  for(let cont = 1; cont <= 10; cont++)
+    result[cont-1] = cont * 5;
+  return NextResponse.json(
+    {
+      "resultado": result
+    }
+  );
+}
